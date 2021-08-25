26be82ce41a3d2187aaca24c64cbfc4210d1aa93
Денис Кольцов <dkolcov@mmtr.ru>
2021-08-25 17:46:19 +0300

    Change fonts

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,20 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './Fonts/fonts.css';
 import {Box} from "@material-ui/core";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-          <div style={{ width: '100%', background: '#FFFAEC' }}>
-              <Box display="flex" p={3}>
+          <div style={{ width: '100%' }}>
+              <Box display="flex" p={4}>
+                  <div className="Container-Red"/>
                   <Box p={1} flexGrow={1}>
-                      <div className="Title">
-                          <p>ANASTASIA  RABOTKO</p>
-                      </div>
+                      <div className="Title-name">ANASTASIA<div className="Title-surname">RABOTKO</div></div>
                   </Box>
+
                   <div className="Menu-item">
                       <Box p={1} className="Item">
                           <a className="Home-link">HOME</a>
@@ -28,18 +29,6 @@ function App() {
               </Box>
           </div>
 
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
